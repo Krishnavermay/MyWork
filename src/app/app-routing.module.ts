5fb695c5357488386b260facd3482bbf53b94018
krishna <krishna.verma@i2k2.com>
2022-06-07 15:07:19 +0530

    add new module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,9 +23,22 @@ const routes: Routes = [
       {
         path: 'home',
         loadChildren: () => import('./modules/home-page/home-page.module').then(m => m.HomePageModule)
-      }, {
+      },
+      {
         path: 'signin',
         loadChildren: () => import('./modules/signin/signin.module').then(m => m.SigninModule)
+      },
+      {
+        path: 'payment',
+        loadChildren: () => import('./modules/payment/payment.module').then(m => m.PaymentModule)
+      },
+      {
+        path: 'contact-us',
+        loadChildren: () => import('./modules/contact-us/contact-us.module').then(m => m.ContactUsModule)
+      },
+      {
+        path: 'about',
+        loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule)
       }
     ]
   },
